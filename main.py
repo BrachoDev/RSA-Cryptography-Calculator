c528f4192c6b88fcb6eb5ba2714369aa8660308b
Carlos Bracho <brachocarlos152@gmail.com>
2024-10-24 18:00:12 -0400

    modified code

--- main.py
+++ main.py
@@ -1,103 +1,70 @@
-# Initializing variables and lists
+import string
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Initializing variables and lists
+alphabet = list(string.ascii_uppercase)
 
 new_number_list = []
 
-# Function to encrypt word
-def encrypt_word():
-    # Word to transform
+# Function to encrypt a word
+def encrypt_word(p, q, e):
     word = input("Insert your phrase: ")
     word_uppercase = word.upper()
-    word_in_list = list(word_uppercase)
-    # Loop that goes through the word's list and prints the index value:
-    for char in word_in_list:
+    
+    for char in word_uppercase:
         if char in alphabet:
             letter_index = alphabet.index(char) + 1
-            get_new_number(p, q, letter_index, e)
+            encrypted_char = encrypt_character(p, q, letter_index, e)
+            new_number_list.append(encrypted_char)
         elif char == " ":
-            new_number_list.append(32)
-    print(new_number_list)
+            new_number_list.append(32)  # Space representation
+    
+    print("Encrypted values:", new_number_list)
 
-# Function to get new encoded number
-def get_new_number(p,q,m,e):
+# Function to encrypt a character
+def encrypt_character(p, q, m, e):
     n = p * q
-    c = (m ** e) % n
-    new_number_list.append(c)
+    return pow(m, e, n)  # Efficient modular exponentiation
 
-# Function to decode word
-def decode_phrase(p,q,e):
-    # Function to compute the greatest common divisor using the Euclidean algorithm
-    def gcd(a, b):
-        while b != 0:
-            a, b = b, a % b
-        return a
-
-    # Function to compute the modular inverse using the Extended Euclidean Algorithm
+# Function to decode a phrase
+def decode_phrase(p, q, e):
     def mod_inverse(e, phi_n):
-        # e * d ≡ 1 (mod phi_n), we need to find d
         old_r, r = e, phi_n
         old_s, s = 1, 0
-        old_t, t = 0, 1
         
-        # Extended Euclidean Algorithm
         while r != 0:
             quotient = old_r // r
             old_r, r = r, old_r - quotient * r
             old_s, s = s, old_s - quotient * s
-            old_t, t = t, old_t - quotient * t
         
-        # The modular inverse is old_s (which is the value of d)
-        # but we need to ensure it is positive
         if old_s < 0:
             old_s += phi_n
         return old_s
 
-    # Compute n and phi(n)
     n = p * q
     phi_n = (p - 1) * (q - 1)
-
-
-    # Calculate the private key d
     d = mod_inverse(e, phi_n)
 
-    encoded_number = input('Input number separated by ",": ').upper()
-    list_encoded_number = encoded_number.split(',')
+    encoded_numbers = input('Input numbers separated by commas: ').split(',')
     decoded_list = []
-    get_new_decoded_number(list_encoded_number,d,n,decoded_list)
-
-    for number in decoded_list:
-        if number <= 26:
-            print(alphabet[number-1])
-        
-        elif number > 26:
-            print(" ")
-
-# Function to get new decoded number
-def get_new_decoded_number(list_encoded_number,d,n,decoded_list):
-    for number in list_encoded_number:
-        m = int(number)
-        decoded_number = (m ** d) % n
+    
+    for number in encoded_numbers:
+        decoded_number = decrypt_character(int(number), d, n)
         decoded_list.append(decoded_number)
+    
+    decoded_message = ''.join([alphabet[num-1] if 1 <= num <= 26 else ' ' for num in decoded_list])
+    print("Decoded message:", decoded_message)
 
+# Function to decrypt a character
+def decrypt_character(c, d, n):
+    return pow(c, d, n)  # Efficient modular exponentiation
 
+# Main execution flow
+p = int(input("Input prime p: "))
+q = int(input("Input prime q: "))
+e = int(input("Input public key e: "))
 
-
-
-# Initial values:
-
-# Prime numbers p and q
-p = int(input("Input p value: "))
-
-q = int(input("Input q value: "))
-
-# Public key
-
-e = int(input("Input public key: "))
-
-# Menu
-user_options = input("Choose 1 to Encrypt or 2 to decode ")
-if user_options == "1":
-    encrypt_word()
-elif user_options == "2":   
-    decode_phrase(p,q,e)
+user_choice = input("Choose 1 to Encrypt or 2 to Decode: ")
+if user_choice == "1":
+    encrypt_word(p, q, e)
+elif user_choice == "2":
+    decode_phrase(p, q, e)
